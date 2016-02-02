64dfc2b09ba056e8f3c3bcd1a1c2bc7b9bec3808
alexjhannan <alexjhannan@gmail.com>
2016-02-02 13:14:32 -0500

    second iteration. instead of logging answers, the mental math function now returns a steps array which can be iterated through for each step of the process. also added a check for trivial multiplication (something * 0) which keeps all trivial steps out of the steps array.

--- client/js/main.js
+++ client/js/main.js
@@ -2,17 +2,17 @@
 
 	function mentalMultiply(num1, num2){
 
+		var steps = [];
+
 		if (num1 > 500 || num2 > 500){
-			console.log('You might need a pen for this one. Write down anything don\'t want to remember.');
+			steps.push('You might need a pen for this one.');
 		}
 
-		console.log('Let\'s multiply ' + num2 + ' by ' + num1 + '.');
+		steps.push('Rather than doing one complicated multiplcation problem, we\'re going to do lots of easy multiplication and addition. This is how mathematicians think.');
 
-		console.log('Rather than doing one complicated multiplcation problem, we\'re going to do lots of easy multiplication and addition. This is how mathematicians think.');
-
-		console.log('First, decompose the numbers into simpler parts:');
-		console.log('\t' + num2 + ' is ' +  decompose(num2).join(' + ') + '.');
-		console.log('\t' + num1 + ' is ' +  decompose(num1).join(' + ') + '.');
+		steps.push('First, decompose the numbers into simpler parts:');
+		steps.push('\t' + num2 + ' is ' +  decompose(num2).join(' + ') + '.');
+		steps.push('\t' + num1 + ' is ' +  decompose(num1).join(' + ') + '.');
 
 
 		// decompose the two numbers into value arrays (this is how the numbers should be 'seen' by the mathematician)
@@ -22,31 +22,37 @@
 		// we need two more arrays to store info in (this is the actual mental model, and what needs to be remembered)
 		var sums = [], products;
 
-		console.log('Now, we can multiply our bits together. Mathematicians know that the sum of the parts is equal to the whole. Multiplying parts together, then, is the same as multiplying wholes together - so long as we do it right.');
+		steps.push('Now, we can multiply our bits together. Mathematicians know that the sum of the parts is equal to the whole. Multiplying parts together, then, is the same as multiplying wholes together - so long as we do it right.');
 
 		for (var i = 0; i < num1.length; i++ ){
 
 			// prepare products array
 			products = [];
 
+			steps.push('Multiply ' + sumOfArray(num2) + ' by ' + num1[i]);
+
 			for (var j = 0; j < num2.length; j++ ){
 
-				console.log('\tMultiplying ' + num2[j] + ' * ' + num1[i] + ' gives ' + num2[j] * num1[i]);
+				if (num1[i] === 0){
+					steps.push('Wait a second, anything multiplied by zero is zero. Skip this step!');
+					break;
+				}
+
+				steps.push('\tMultiplying ' + num2[j] + ' * ' + num1[i] + ' gives ' + num2[j] * num1[i] + '. (' + num2[j].toString().substring(0, 1) + ' * ' + num1[i].toString().substring(0,1) + ' = ' + num2[j].toString().substring(0,1)*num1[i].toString().substring(0,1) + ')');
 
 				// multiply values together, remembering each product
 				products.push(num1[i] * num2[j]);
 
 			}
+			try {
+				var sum = sumOfArray(products);
+			} catch (err) {
+				var sum = 0;
+			}
 
-			var sum = sumOfArray(products);
+			if (sum) { 
 
-			if (sum === 0) {
-
-				console.log('Sum is zero, so we don\'t need to remember this. A smart mathematician might have foreseen this and skipped this entire step!');
-			
-			} else { 
-
-				console.log('Adding together ' + products.join(' + ') + ' gives us ' + sum +'. Remember this number.'); 
+				steps.push('Adding together ' + products.join(' + ') + ' gives us ' + sum +'. Remember this number.'); 
 
 				sums.push(sum);
 
@@ -56,7 +62,7 @@
 
 		}
 
-		console.log('Remember those ' + sums.length + ' numbers? Adding together ' + sums.join(' + ') + ' gives us ' + sumOfArray(sums) + '. Congratulations, you\'ve just multiplied ' + sumOfArray(num1) + ' * ' + sumOfArray(num2) + '!');
+		steps.push('Remember those ' + sums.length + ' numbers? Adding together ' + sums.join(' + ') + ' gives us ' + sumOfArray(sums) + '. Congratulations, you\'ve just multiplied ' + sumOfArray(num1) + ' * ' + sumOfArray(num2) + '!');
 
 		// 176 decomposes into [100, 70, 6]
 		function decompose(num){
@@ -82,8 +88,11 @@
 				return prev + curr;
 			});
 		}
+
+		return steps;
 	}
 
-	mentalMultiply(9300, 773);
+	console.log(mentalMultiply(9300, 773));
+
 
 }());
